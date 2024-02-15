3aef1fe58b4f6b1944f332b5efedd181bcc31644
DemirelZ <zaferdemirelgsb@gmail.com>
2024-02-15 13:53:29 +0300

    user's choises are added

--- App.jsx
+++ App.jsx
@@ -1,12 +1,91 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+import {COLOR} from './src/utils/constant';
+import {choises} from './src/data/mockData.js';
 
 const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+  const [userChoise, setUserChoise] = useState(null);
+  const [computerChoise, setComputerChoise] = useState(null);
+  const [result, setResult] = useState(null);
 
-export default App
+
+  const handleGame =(oneOfThree)=>{
+
+    
+
+  }
+
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle={'light-content'} />
+      <View style={styles.container}>
+        <Text style={styles.title}>ROCK PAPER SCISSORS</Text>
+        <Text style={styles.usersChoise}>User's Choise</Text>
+        <View style={styles.choises}>
+          {choises?.map(choise => (
+            <TouchableOpacity style={styles.button} onPress={handleGame(choise)}>
+              <Image source={choise.image} style={styles.image} />
+            </TouchableOpacity>
+          ))}
+        </View>
+        <Text style={styles.title}>KAZANDIN!!!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLOR.backgroungcolor,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: '900',
+    marginVertical: 20,
+  },
+  usersChoise: {
+    color: 'white',
+    marginVertical: 20,
+    fontSize: 20,
+  },
+  choises: {
+
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 10,
+  },
+  button:{
+    width: 90,
+    height: 90,
+    alignItems:'center',
+    justifyContent:'center',
+    margin:3,
+    padding:10,
+    backgroundColor:'white',
+    borderRadius:10
+    
+
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+
+    margin: 5,
+  },
+});
--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,4 +1,4 @@
-const choises = [
+export const choises = [
     {id:1, name: 'Taş', image:require('../assets/tas.png')},
     {id:2, name:'Kağıt', image:require('../assets/kagit.png')},
     {id:3, name:'Makas', image: require('../assets/makas.png')}
